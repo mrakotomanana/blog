7a293a6b9fdff2b7b55f498916feb942be1f1e8d
michel rakotomanana <mrakotomanana.vit@gmail.com>
2023-04-26 11:58:28 +0300

    add config env

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,74 @@
 const express = require("express");
 const router = express.Router();
+var sqlite3 = require("sqlite3");
 
-var getData = function () {
-    var data = {
-        'element1': 'https://images.unsplash.com/photo-1563422156298-c778a278f9a5',
-        'element2': 'https://images.unsplash.com/photo-1620173834206-c029bf322dba',
-        'element3': 'https://images.unsplash.com/photo-1602491673980-73aa38de027a'
-    };
-    return data;
-};
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
+  // log each request to the console
+  console.log(req.method, req.url);
 
-    // log each request to the console
-    console.log(req.method, req.url);
-
-    // continue doing what we were doing and go to the route
-    next();
+  // continue doing what we were doing and go to the route
+  next();
 });
 
-// route middleware to validate :name
-router.param('name', function(req, res, next, name) {
-    // do validation on name here
-    // blah blah validation
-    // log something so we know its working
-    console.log('doing name validations on ' + name);
-
-    // once validation is done save the new item in the req
-    req.name = name;
-    // go to the next thing
-    next();
+router.get("/", function (req, res) {
+  console.log(res.body);
+  res.render("index", { title: "Express" });
 });
 
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express', "data": getData() });
+router.get("/lastName/:name", function (req, res) {
+  console.log(res.body);
+  var db = new sqlite3.Database("mydb.db");
+
+  db.serialize(function () {
+    // Create a table
+    db.run(
+      "CREATE TABLE IF NOT EXISTS Foo (id INTEGER PRIMARY KEY, name TEXT, lastname TEXT)"
+    );
+
+    // Insert data into the table
+    // db.run("INSERT INTO Foo (name, lastname) VALUES ('" + req.body.firstName + "', '" + req.body.firstName + "')");
+
+    // Query data from the table
+    db.each("SELECT id, name, lastname FROM Foo", function (err, row) {
+      console.log(row.id + " : " + row.name + " - " + row.lastname);
+    });
+  });
+
+  db.close();
+  res.render("index", { title: "Express" });
 });
 
-router.get('/test/', function (req, res) {
-    res.send("test connextion");
+router.get("/create", function (req, res) {
+  res.render("create", { title: "Create Recorder" });
 });
 
-router.get('/test/:name', function(req, res) {
-    res.send('test ' + req.name + '!');
+router.post("/create", function (req, res) {
+  console.log(req.body);
+  // res.render('create', { title: 'Create Recorder', data : req.body});
+
+  var db = new sqlite3.Database("mydb.db");
+
+  db.serialize(function () {
+    // Create a table
+    db.run(
+      "CREATE TABLE IF NOT EXISTS Foo (id INTEGER PRIMARY KEY, name TEXT, lastname TEXT)"
+    );
+
+    // Insert data into the table
+    db.run("INSERT INTO Foo (name, lastname) VALUES ('" + req.body.firstName + "', '" + req.body.lastName + "')");
+
+    // Query data from the table
+    // db.each("SELECT id, name FROM Foo", function (err, row) {
+    //   console.log(row.id + ": " + row.name);
+    // });
+  });
+
+  db.close();
+
+  res.redirect(303, '/app/lastName/' + req.body.lastName);
+
+//   res.render("index", { title: "Express" });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,90 +1,29 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const path = require('path');
 
-const mongoDB = "mongodb://127.0.0.1/my_database";
+const path = require("path");
 const router = require("./routes/index");
-const parkings = require("./parkings.json");
-const { Dog } = require("./models/models");
+const config =  require('./config.js');
 
-const port = 8000;
+const port = config.PORT;
 const app = express();
 
-app.use(express.static("public"));
-app.set("view engine", "pug");
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+app.set("view engine", "pug");
 app.set("views", "./views");
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.use("/app", router);
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-app.get("/parkings", (req, res) => {
-  res.status(200).json(parkings);
-});
-
-app.get("/dogs", async (req, res) => {
-  try {
-    await mongoose.connect(mongoDB);
-    const allDogs = await Dog.find();
-    await mongoose.connection.close();
-    res.status(200).json(allDogs);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false });
-  }
-});
-
-app.get("/dogs/:name/:breed/:age/:isGoodBoy", async function (req, res) {
-  try {
-    await mongoose.connect(mongoDB);
-    const nameFirst = req.params.name;
-    const breed = req.params.breed;
-    const age = req.params.age;
-    const isGoodBoy = req.params.isGoodBoy;
-    const newDog = new Dog({
-      name: nameFirst,
-      breed: breed,
-      age: age,
-      isGoodBoy: isGoodBoy,
-    });
-    await newDog.save();
-    await mongoose.connection.close();
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false });
-  }
-});
-
-app.get("/parkings/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const parking = parkings.filter((parking) => parking.id == id);
-  console.log(parking);
-  console.log(id);
-  console.log(parking[0]["id"]);
-  let str = parking[0].id;
-  res.status(200).json({ id: str });
-});
-
-app.route("/login").get(function (req, res) {
-  res.send("this is the login formsdf");
-});
-
-app.route("/login").post(function (req, res) {
-  console.log("processing");
-  res.send("processing the login form!");
-});
-
 const start = async () => {
   try {
-    // await mongoose.connect(mongoDB);
-    // console.log(mongoose.connections.length);
-    // mongoose.createConnection(mongoDB);
     app.listen(port, () => {
-      console.log(`Listening on ${port}`);
+      console.log(`Listening on http://${config.HOST}:${port}`);
     });
   } catch (error) {
     console.error(error);
